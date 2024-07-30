83595854539885dba1d0b0a926fddd463d7f0444
MiquelAngelMontero <97508353+MiquelAngelMontero@users.noreply.github.com>
2024-07-30 08:22:01 +0200

    API ok

--- src/main/java/com/example/demo/dao/ITareaDAO.java
+++ src/main/java/com/example/demo/dao/ITareaDAO.java
@@ -1,10 +1,20 @@
 package com.example.demo.dao;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.dto.Tarea;
 
 public interface ITareaDAO extends JpaRepository<Tarea, Integer>{
 
+	@Query("SELECT t FROM Tarea t WHERE t.grua.id = :gruaId AND " +
+	           "(t.fecha_inicio < :fin AND t.fecha_final > :inicio)")
+	List<Tarea> findOverlappingTareas(@Param("gruaId") int gruaId,
+	                                  @Param("inicio") LocalDateTime inicio,
+	                                  @Param("fin") LocalDateTime fin);
 	
 }
--- src/main/java/com/example/demo/service/TareaServiceIMPL.java
+++ src/main/java/com/example/demo/service/TareaServiceIMPL.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,16 @@ public class TareaServiceIMPL implements ITareaService {
 	@Override
 	public Tarea guardarTarea(Tarea Tarea) {
 		// TODO Auto-generated method stub
+        LocalDateTime fechaFinal = Tarea.getFecha_inicio().plus(Tarea.getDuracion(), ChronoUnit.MINUTES);
+        Tarea.setFecha_final(fechaFinal);
+		
+		List<Tarea> overlappingTareas = iTareaDAO.findOverlappingTareas(
+                Tarea.getGrua().getId(), Tarea.getFecha_inicio(), Tarea.getFecha_final());
+
+        if (!overlappingTareas.isEmpty()) {
+            throw new IllegalStateException("La grúa ya está ocupada en el periodo de tiempo especificado.");
+        }
+		   
 		return iTareaDAO.save(Tarea);
 	}
 
